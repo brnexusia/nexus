698d38b685747a79c6d647a2983d74d0c1fb0fd6
brnexusia <brnexusia@gmail.com>
2026-09-01 20:40:25 -0300

    feat(i18n): set Chat Arles dashboard to pt-BR

--- app/javascript/entrypoints/dashboard.js
+++ app/javascript/entrypoints/dashboard.js
@@ -36,7 +36,8 @@ import 'floating-vue/dist/style.css';
 
 const i18n = createI18n({
   legacy: false, // https://github.com/intlify/vue-i18n/issues/1902
-  locale: 'en',
+  locale: 'pt_BR',
+  fallbackLocale: 'pt_BR',
   messages: i18nMessages,
   postTranslation: replaceVisibleBrandNamesInTranslation,
 });
@@ -69,7 +70,7 @@ if (window.errorLoggingConfig) {
       /safari-web-extension:/i,
       /safari-extension:/i,
     ],
-    integrations: [Sentry.browserTracingIntegration({ router })],
+    integrations: [Sentry.browserTracingIntegration({ router }),],
     ignoreErrors: [
       'ResizeObserver loop completed with undelivered notifications',
     ],
